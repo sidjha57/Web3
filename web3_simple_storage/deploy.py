bc073c7a3cc5d04ae0fca1013628d9b1b00a7d55
Siddharth <sidjha57@gmail.com>
2021-10-31 21:04:19 +0530

    d

--- web3_simple_storage/deploy.py
+++ web3_simple_storage/deploy.py
@@ -1,7 +1,10 @@
 from solcx import compile_standard, install_solc  # <- import the install_solc method!
 import json
 from web3 import Web3
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 install_solc("0.6.0")  # <- Add this line and run it at least once!
 
 with open("SimpleStorage.sol", "r") as file:
@@ -21,7 +24,7 @@ compiled_sol = compile_standard(
     solc_version="0.6.0",
 )
 
-print("compiled_sol")
+#print("compiled_sol")
 
 with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file)
@@ -37,10 +40,26 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # for connecting to ganache
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-chain_id = 5777
-my_address = "0x7Da1857970d86b44a74d32D4dc24361E4112f6C6"
-private_key = "0xa0a25561d3cdd8d6e4f18bf04372043a02a9db52be127037259b345d9821a17c"
+chain_id = 1337
+my_address = "0x2bf460e66FaA9056290363676feC317806723458"
+#private_key = "0xb9b824cde38c2abfdf9a447803f48d8201433f57976e0473f7c4171185d7f515"
+private_key = os.getenv("PRIVATE_KEY")
 
 # Create the contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
-print(SimpleStorage)
+#print(SimpleStorage)
+
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+#print(nonce)
+
+# 1. Build a transaction
+# 2. Sign a transaction
+# 3. Send a transaction
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+)
+#print(transaction)
+
+signed_tcn = w3.eth.account.sign_transaction(transaction, private_key = private_key)
+print(private_key)
